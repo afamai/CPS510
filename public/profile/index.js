065dfde58c215e212d19b1ec5900dc1a03e370d1
Kevin <k.nguyenphams@gmail.com>
2018-11-22 18:45:48 -0500

    Added queries for employee and contact id

--- public/profile/index.js
+++ public/profile/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var oracle = require('../../server/service/oracle');
+var oracle = require('../../services/oracle');
 
 //required for post?
 var bodyParser = require('body-parser');
@@ -11,16 +11,34 @@ router.use(bodyParser.urlencoded({
 // router.use(express.json());
 // router.use(express.urlencoded());
 
+router.get('/profile', function(req, res) {
+    res.sendFile( 'profile.html', { root: __dirname })
+});
+
+
 router.post('/profile', function(req, res) {
-    result = oracle.runSql('select * from login');
-    result.then(function(r){
-        for (var i = 0; i < r.rows.length; i++) {
-            console.log(i + ']' + r.rows[i]);
-        }
-    });
-    
-    res.write('hello');
-    // res.write(req.body.user + req.body.pass);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var conQuery =
+        'INSERT INTO contact (id, address, phone, email) VALUES (' +
+        // req.body.contactid + ',' +
+        '\'' + req.body.address + '\',' +
+        '\'' + req.body.phone + '\',' +
+        '\'' + req.body.email + '\'' +
+        ')';
+    var empQuery =
+        'INSERT INTO employee (id, firstname, lastname, dateofbirth, sex, role, managerid, contactid) VALUES (' +
+        // req.body.employeeid + ',' +
+        '\'' + req.body.firstname + '\',' +
+        '\'' + req.body.lastname + '\',' +
+        'TO_DATE(\'' + req.body.birth + '\', \'YYYY-MM-DD\') ,' +
+        '\'' + req.body.sex + '\',' +
+        '\'' + req.body.role + '\',' +
+        req.body.managerid + ',' +
+        // req.body.contactid +
+        ')';
+    // result = oracle.runSql();
+    res.write('<h1>' + conQuery + '</h1><br>');
+    res.write('<h1>' + empQuery + '</h1>');
     res.end();
 });
 
