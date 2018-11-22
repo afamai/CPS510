b5ca4e71c370200268f512ef3dbb4b2cb8e51621
Alpha Mai <alpha.mai@ryerson.ca>
2018-11-22 01:27:21 -0500

    temporary remove templating and add routing

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 const express = require('express');
-const tmp = require('./template')
 
 //init
 var oracle = require('./server/service/oracle.js')
 const app = express();
 const port = 80;
 
-
-app.get('/', function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    tmp.loadHTML(res, '/header.html', '/index.html', '/footer.html');
-});
+app.use(require('./views'));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 // example. REMOVE LATER
 result = oracle.runSql('select table_name from user_tables');
 result.then(function(r){
     console.log(r);
-})
+});
