1099510868a5b79e76b038de6a8e196da9ed582f
Kevin <k.nguyenphams@gmail.com>
2018-11-23 00:14:39 -0500

    working insert

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 80;
 
 app.use(express.static('public'));
-app.use(require('./public/employees'));
+app.use(require('./public/home'));
+app.use(require('./public/profile'));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
--- public/profile/index.js
+++ public/profile/index.js
@@ -20,61 +20,19 @@ router.get('/profile', function (req, res) {
 router.post('/profile', function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    //create Query for new contact
-    var conQuery =
-        'INSERT INTO contact (id, address, phone, email) VALUES (' +
-        'contact_id_seq.nextval ,' +
-        '\'' + req.body.address + '\',' +
-        '\'' + req.body.phone + '\',' +
-        '\'' + req.body.email + '\'' +
-        ')';
-    var newContact = contact.model();
+    //create model for new contact
+    var newContact = contact.model;
     newContact.address = req.body.address;
     newContact.phone = req.body.phone;
     newContact.email = req.body.email;
-
-    contact.insertContact(contactmodel)
-
-
-
-
-    //add contact to db
-    // oracle.runSql(conQuery);
-
-    // var contactSel = oracle.runSql('SELECT * FROM contact');
-    // contactSel.then(function (r) {
-    //     console.log('This is Contact Query: ')
-    //     console.log(r);
-    // });
-
-    //get current contactid value
-    // var result = oracle.runSql('SELECT contact_id_seq.currval FROM DUAL');
-    // result.then(function (r) {
-    //     console.log('This is Current Value: ')
-    //     console.log(r);
-    //     // for (var i = 0; i < r.rows.length; i++) {
-    //     //     console.log(i + ']' + r.rows[i]);
-    //     // }
-    // });
-
-    //create Query for new employee
-    var empQuery =
-        'INSERT INTO employee (id, firstname, lastname, dateofbirth, sex, role, managerid, contactid) VALUES (' +
-        'employeee_id_seq.nextval ,' +
-        '\'' + req.body.firstname + '\',' +
-        '\'' + req.body.lastname + '\',' +
-        'TO_DATE(\'' + req.body.birth + '\', \'YYYY-MM-DD\') ,' +
-        '\'' + req.body.sex + '\',' +
-        '\'' + req.body.role + '\',' +
-        req.body.managerid + ',' +
-        // req.body.contactid +
-        ')';
+    //insert new contact
+    contact.insertContact(newContact);
 
     var loginQuery;
     // 'INSERT INTO Login (EmployeeID, Password, lastlogin) VALUES (1, '5f4dcc3b5aa765d61d8327deb882cf99', TO_DATE('2018-11-02 09:34:33', 'YYYY-MM-DD HH24:MI:SS'));'
     // result = oracle.runSql();
-    res.write('<h1>' + conQuery + '</h1><br>');
-    res.write('<h1>' + empQuery + '</h1>');
+    // res.write('<h1>' + conQuery + '</h1><br>');
+    // res.write('<h1>' + empQuery + '</h1>');
     res.end();
 });
 
