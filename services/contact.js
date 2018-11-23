7be6f63637b6b518f95ed3d97228efd7211464b6
Alpha Mai <alpha.mai@ryerson.ca>
2018-11-23 01:49:01 -0500

    Able to add employees to database

--- services/contact.js
+++ services/contact.js
@@ -1,21 +1,25 @@
 var oracle = require('./oracle.js');
-
+var util = require('util');
 module.exports = {
-    insertContact: async function (contactModel) {
-        oracle.open().then(function (c) {
-            var query = oracle.runSql(
-                'INSERT INTO contact (id, address, phone, email) VALUES (\'contact_id_seq.nextval\', )', c);
+    addContact: async function (address, phone, email, callback) {
+        // create sql
+        var sql = util.format("INSERT INTO contact (id, address, phone, email) VALUES (contact_id_seq.nextval, '%s', '%s', '%s')", address, phone, email);
+        oracle.open().then(function (conn) {
+            // run sql to insert new sql
+            var query = oracle.runSql(sql, conn);
             query.then(function (result) {
-                callback(result);
-                oracle.close(c);
+                // run 2nd sql to get contact id
+                var getContactId = oracle.runSql('select contact_id_seq.currval from DUAL', conn);
+                getContactId.then(function(r){
+                    if(callback){
+                        // run callback to perform additional queries
+                        callback(r.rows[0].CURRVAL, conn);
+                    } else {
+                        oracle.close(conn);
+                    }
+                })
             })
         });
-    },
-
-    model() {
-        this.adress;
-        this.phone;
-        this.email;
     }
 
 }
--- services/employees.js
+++ services/employees.js
@@ -1,5 +1,6 @@
 var oracle = require('./oracle.js');
-
+var contact = require('./contact');
+var util = require('util');
 module.exports = {
     getEmployees: async function(callback){
         oracle.open().then(function(c){ 
@@ -9,5 +10,37 @@ module.exports = {
                 oracle.close(c);
             })
         });
+    },
+    addEmployee: async function(employee, callback){
+        //create sql
+        var withManagerTemp = "INSERT INTO employee(id, firstname, lastname, dateofbirth, sex, role, contactid, managerid) \
+VALUES (employee_id_seq.nextval, '%s', '%s', TO_DATE('%s', 'YYYY-MM-DD'), '%s', '%s', %d, %d)"
+        var noManagerTemp = "INSERT INTO employee(id, firstname, lastname, dateofbirth, sex, role, contactid) \
+VALUES (employee_id_seq.nextval, '%s', '%s', TO_DATE('%s', 'YYYY-MM-DD'), '%s', '%s', %d)"          
+        contact.addContact(employee.address, employee.phone, employee.email, function(contactid, conn){
+            let sql;
+            if(employee.managerid == ''){
+                sql = util.format(noManagerTemp,  employee.firstname, employee.lastname, employee.birth, employee.sex, employee.role, contactid);
+            } else{
+                sql = util.format(withManagerTemp,  employee.firstname, employee.lastname, employee.birth, employee.sex, employee.role, contactid, employee.managerid);
+            }
+            var query = oracle.runSql(sql, conn);
+            query.then(function(result){
+                var getEmployeeId = oracle.runSql("select employee_id_seq.currval from DUAL", conn);
+                getEmployeeId.then(function(r){
+                    employeeid = r.rows[0].CURRVAL;
+                    addPassword(employeeid, employee.password, conn);
+                    callback();
+                });
+            })
+        });
     }
+}
+
+var addPassword = async function(employeeid, password, conn){
+    var sql = util.format("INSERT INTO login(employeeid, password) VALUES (%d, '%s')", employeeid, password);
+    var query = oracle.runSql(sql, conn);
+    query.then(function(){
+        oracle.close(conn);
+    });
 }
