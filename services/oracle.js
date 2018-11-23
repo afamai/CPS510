1e867e13d217e1af614c8615edd9190e37a2a9cc
Alpha Mai <alpha.mai@ryerson.ca>
2018-11-22 22:17:30 -0500

    modified oracle.js

--- services/oracle.js
+++ services/oracle.js
@@ -1,32 +1,40 @@
 var oracledb = require('oracledb');
 var config = require('../config/dbconfig');
 
-// modeule.exports allow function to be used in other files
+//modeule.exports allow function to be used in other files
 module.exports = {
-    runSql: async function(sql) {
-        let connection, result;
-        try {
-            // init connection
+    open: async function(){
+        // init connection
+        let connection;
+        try{
             connection = await oracledb.getConnection(  {
                 user          : config.user,
                 password      : config.pass,
                 connectString : config.connectionStr
             });
-            
+        } catch(err){
+            console.log(err);
+        }
+        return connection;
+    },
+    runSql: async function(sql, connection) {
+        try {
             // execute sql
             result = await connection.execute(sql);
         } catch (err) {
             console.error(err);
-        } finally {
-            if (connection) {
-                try {
-                    await connection.close();
-                } catch (err) {
-                    console.error(err);
-                }
-            }
         }
         // return a promise
         return result;
+    },
+    close: async function(connection){
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+            console.log('closed');
+        }
     }
 }
