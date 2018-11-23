2b41d20c5710587b12adc64f820704d11a9c048d
Alpha Mai <alpha.mai@ryerson.ca>
2018-11-22 23:12:38 -0500

    forgot to delete

--- public/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', require('./home'));
-
-router.get('/profile', require('./profile'));
-router.post('/profile', require('./profile'));
-
-module.exports = router
