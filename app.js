3bd9be88d27bc3c185d624d7b75d990ea599ea90
Alpha Mai <alpha.mai@ryerson.ca>
2018-11-22 23:12:14 -0500

    modified routing to work properly

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-app.set('view engine', 'pug')
 app.use(express.static('public'));
-app.use(require('./public'));
+app.use(require('./public/employees'));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
