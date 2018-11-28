f3b5ed0374cf17d2fd0ffb692262f0fef3ce5e7e
Alpha Mai <alpha.mai@ryerson.ca>
2018-11-28 14:51:57 -0500

    Allow viewing and editing of vendor profile

--- public/vendor/index.js
+++ public/vendor/index.js
@@ -14,6 +14,21 @@ router.get('/vendor', function(req, res) {
     });
 });
 
+router.get('/vendor/profile/:id', function(req, res) {
+    db.getVendorProfile(req.params.id, function(result){
+        var view = result.rows[0];
+        view["ID"] = req.params.id;
+        var html = fs.readFileSync("./public/vendor/profile.html", 'utf8');
+        var output = mustache.render(html, view);
+        res.send(output);
+    });
+});
+router.post('/vendor/profile/update', function(req, res) {
+    console.log(req.body);
+    db.updateVendor(req.body, function(){
+        res.redirect('/vendor/profile/'+req.body.id);
+    })
+});
 router.post('/vendor/add', function(req, res) {
     db.addVendor(req.body, function () {
         res.redirect('/vendor');
--- services/vendor.js
+++ services/vendor.js
@@ -2,7 +2,6 @@ var oracle = require('./oracle.js');
 var contact = require('./contact');
 var util = require('util');
 module.exports = {
-
     addVendor: async function (vendor, callback) {
         //create sql
         var sqlTemp = "INSERT INTO vendor(id, companyname, contactid) VALUES (vendor_id_seq.nextval, '%s', %d)";
@@ -18,6 +17,27 @@ module.exports = {
             query.then(function(result){
                 oracle.close(c);
                 callback(result);
+            });
+        });
+    },
+    getVendorProfile: async function(id, callback){
+        oracle.open().then(function(conn){
+            var sql = "select * FROM vendor v, contact c WHERE v.contactid = c.id and v.id=" + id;
+            oracle.runSql(sql, conn).then(function(result){
+                oracle.close(conn);
+                callback(result);
+            });
+        });
+    },
+    updateVendor: async function(vendor, callback){
+        var sqlTemp = "UPDATE vendor SET companyname=%s WHERE id=%d"
+        oracle.open().then(function(conn){
+            var sql = util.format(sqlTemp, vendor.companyname, vendor.id);
+            oracle.runSql(sql, conn).then(function(){
+                contact.updateContact(vendor.contactid, vendor.address, vendor.phone, vendor.email, conn, function(){
+                    oracle.close(conn);
+                    callback();
+                })
             });
         });
     }
