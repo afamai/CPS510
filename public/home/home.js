c8e0af42f4dc4d6ec04940ba7c17158c973beb4d
Alpha Mai <alpha.mai@ryerson.ca>
2018-11-28 14:25:40 -0500

    changed home to redirect properly

--- public/home/home.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('#employee').on('click', function(){
-        window.location = "/employees"
-    });
-
-    $('#item').on('click', function(){
-        window.location = "/item"
-    });
-});
